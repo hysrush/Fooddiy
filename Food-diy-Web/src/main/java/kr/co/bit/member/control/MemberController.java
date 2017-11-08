cbc1a8ff3937358fee8df60c65366dcf5ff40b29
RYU <bit-user@DESKTOP-C1QR4BV>
2017-11-08 15:49:55 +0900

    d

--- Food-diy-Web/src/main/java/kr/co/bit/member/control/MemberController.java
+++ Food-diy-Web/src/main/java/kr/co/bit/member/control/MemberController.java
@@ -1,33 +1,24 @@
 package kr.co.bit.member.control;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import kr.co.bit.member.service.MemberService;
-import kr.co.bit.member.vo.MemberVO;
 
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 		
-		@Autowired
-		private MemberService memberService;
-		
-		@RequestMapping("list.do")
-		public ModelAndView list() {
-			List<MemberVO>memberList=memberService.selectAllmember();
-			
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("member/Latest-Order");
-			mav.addObject("memberList", memberList);
-			
-			return mav;
-			
+	    
+	 @RequestMapping(value="/memberDetail")
+		public String memberDetail(Model model,HttpSession httpsession) {
+			return "memberDetail";
 		}
-}
+
+
+	}
+	 
+
 		
