ea41e6104651e51490438220656788593462eb46
SMJ <smj91121@naver.com>
2017-11-10 10:34:37 +0900

    qna 관련 수정 / 이벤트 오류 고침!

--- Food-diy-Web/src/main/java/kr/co/bit/community/service/QnaServiceImp.java
+++ Food-diy-Web/src/main/java/kr/co/bit/community/service/QnaServiceImp.java
@@ -12,41 +12,41 @@ import kr.co.bit.community.vo.QnaBoardVO;
 public class QnaServiceImp implements QnaService {
 
 	@Autowired
-	private QnaDAO communityDAO;
+	private QnaDAO qnaDAO;
 	
 	// <QnA Service>
 	// QnA 전체보기
 	@Override
 	public List<QnaBoardVO> selectAllQnA() {
-		List<QnaBoardVO> listAll = communityDAO.selectAll();
+		List<QnaBoardVO> listAll = qnaDAO.selectAll();
 		return listAll;
 	}
 	// 타입별 QnA 전체보기
 	@Override
 	public List<QnaBoardVO> selectTypeQnA(String type) {
-		List<QnaBoardVO> listType = communityDAO.selectType(type);
+		List<QnaBoardVO> listType = qnaDAO.selectType(type);
 		return listType;
 	}
 	// 글번호별 QnA 글보기
 	@Override
 	public QnaBoardVO selectOneQnA(int no) {
-		QnaBoardVO qnaDetail = communityDAO.selectOne(no);
+		QnaBoardVO qnaDetail = qnaDAO.selectOne(no);
 		return qnaDetail;
 	}
 	// QnA 새 글쓰기
 	@Override
 	public void insertQnA(QnaBoardVO qna) {
-		communityDAO.insert(qna);
+		qnaDAO.insert(qna);
 	}
 	// QnA 글 수정
 	@Override
 	public void modifyQnA(QnaBoardVO qna) {
-		communityDAO.update(qna);
+		qnaDAO.update(qna);
 	}
 	// QnA 글 삭제
 	@Override
 	public void removeQnA(int no) {
-		communityDAO.delete(no);
+		qnaDAO.delete(no);
 	}
 
 	
