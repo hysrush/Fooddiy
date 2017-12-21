014c5fb7319c84ac65daa377ee3cddd6493f3f49
SYC <bit-user@DESKTOP-C1QR4BV>
2017-12-21 14:44:24 +0900

    dd

---
+++ Food-diy-Web/src/main/java/kr/co/bit/service/CartService.java
@@ -0,0 +1,19 @@
+package kr.co.bit.service;
+
+import java.util.List;
+
+import kr.co.bit.vo.CartVO;
+
+public interface CartService {
+	
+	List<CartVO> selectAllCart(CartVO cartVO);
+	CartVO selectOneCart();
+	void insertCart(CartVO cartVO);
+	void updateCart(CartVO cartVO);
+	void updateProductQty(CartVO cartVO);
+	void deleteCartById(String id);
+	void deleteCartByNo(CartVO cartVO);
+	//최근주문 내역
+	List<CartVO> selectMenu(String id);
+	List<CartVO> cartDelete(CartVO vo);
+}
---
+++ Food-diy-Web/src/main/java/kr/co/bit/service/CartStoreServiceImp.java
@@ -0,0 +1,36 @@
+package kr.co.bit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.bit.dao.CartStoreDAO;
+import kr.co.bit.vo.CartStoreVO;
+
+
+@Service
+public class CartStoreServiceImp implements CartStoreService{
+	
+	@Autowired
+	CartStoreDAO dao;
+	
+	@Override
+	public CartStoreVO selectOneCartStore(String id){
+		CartStoreVO cartStoreVO = dao.selectOne(id);
+		return cartStoreVO;
+	}
+	
+	@Override
+	public void insertCartStore(CartStoreVO cartStoreVO) {
+		dao.insert(cartStoreVO);
+	}
+	
+	@Override
+	public void updateCartStore(CartStoreVO cartStoreVO) {
+		dao.update(cartStoreVO);
+	}
+	@Override
+	public void deleteCartStore(String id) {
+		dao.delete(id);
+	}
+	
+}
